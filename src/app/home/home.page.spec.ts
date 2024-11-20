425dd6b6a977e2945a0170c1976b7de7728b4703
robimess <max.gatica@duocuc.cl>
2024-11-20 18:07:39 -0300

    tercer commit: pruebas del homepage ok

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -21,4 +21,25 @@ describe('HomePage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('deberia sumar 2 numeros', () => {
+    expect(component.sumar(1, 1)).toBe(2);
+  })
+  it('deberia mostrar error por ingreso de numeros negativos', () => {
+    expect(() => component.sumar(1, -1)).toThrowError('Error: solo numero positivos');
+  })
+  it('deberia restar 2 numeros', () => {
+    expect(component.restar(1, 1)).toBe(0);
+  })
+  it('deberia multiplicar 2 numeros', () => {
+    expect(component.multiplicar(1, 1)).toBe(1);
+  })
+  it('deberia dividir 2 numeros', () => {
+    expect(component.dividir(1, 1)).toBe(1);
+  })
+  it('deberia mostrar error de divison por cero', () => {
+    expect(() => component.dividir(1, 0)).toThrowError('Error: division por cero')
+  })
+  it('deberia coincidir el titulo de HomePage',()=>{
+    expect(component.title).toBe('HomePage')
+  })
 });
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  title:string= "HomePage"
+  constructor() { }
 
-  constructor() {}
+  sumar(a: number, b: number): number {
+    if (a < 0 || b < 0) {
+      throw new Error('Error: solo numero positivos')
+    }
+    return a + b
+  }
+  restar(a:number,b:number):number{
+    return a - b
+  }
+  multiplicar(a:number,b:number):number{
+    return a * b
+  }
+  dividir(a:number,b:number):number{
+    if (b==0) {
+      throw new Error('Error: division por cero')
+    }
+    return a / b
+  }
 
 }
